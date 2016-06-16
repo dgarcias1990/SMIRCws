a4b85d34c17a0a3779d303ea152b00f460598396
Diana Saldaña <dianasaldana@MacBook-Pro-de-Diana-Saldana.local>
2016-06-16 00:17:39 -0500

    ultimos cambios

--- wsSMIRCTT/urls.py
+++ wsSMIRCTT/urls.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 from django.conf.urls import include, url
-from wsSMIRCauth.views import wsLogin, wsLocationsRegister,wsLogout
+from wsSMIRCauth.views import wsLogin, wsLocationsRegister,wsLogout,wsRutaMaps,showMap
 
 urlpatterns = [
     url(r'^wsLogin/$', wsLogin,name='wsLogin'),
     url(r'^wsRegisterLog/$', wsLocationsRegister,name='RegisterLogs'),
     url(r'^wsLogout/$', wsLogout,name='Logout'),
-  
+    url(r'^Maparuta/$',wsRutaMaps, name='ruta'),
+    url(r'^wsrutaUsuario/$',saveDatatoRoute,name="rutaUsuario"),
 ] 
--- wsSMIRCauth/views.py
+++ wsSMIRCauth/views.py
@@ -53,3 +53,59 @@ def wsLogout(request):
 	InicioUserapp.objects.filter(email=usuario).update(sesionactiva=False)
 	resp={'codigo':'logout'}
 	return HttpResponse(json.dumps(resp), content_type="application/json")
+
+def wsRutaMaps(request):
+	data=json.loads(request.body)
+	instanceUser=InicioUserapp.objects.all().get(id=data['id'],email=data['usuario'])
+
+	if instanceUser:
+		routeCoords=[]
+		for item in data['locations']:
+			routeCoords.append({'lon': item['lon'], 'lat': item['lat']})
+
+	print routeCoords
+
+	mapasCode="""
+		function initMap() {
+  			var map = new google.maps.Map(document.getElementById('mapCanvas'), {
+    		zoom: 12,
+    		center: {lat: 0, lng: -180},
+   		mapTypeId: google.maps.MapTypeId.MAPROAD
+   		});
+		var flightPath = new google.maps.Polyline({
+    	path: [%s],
+    	geodesic: true,
+    	strokeColor: '#FA5882',
+    	strokeOpacity: 1.0,
+    	strokeWeight: 2
+  		});
+		flightPath.setMap(map);
+		}
+		"""% (routeCoords)
+
+	return render(request,"RutasMaps.html",{'inicializar':mapasCode})
+
+def saveDatatoRoute(request):
+	try:
+		data=json.loads(request.body)
+		instanceUser=InicioUserapp.objects.all().get(id=data['id'],email=data['usuario'])
+		for item in data['locations']:
+			instance=InicioLocalization()
+			instance.usuario=instanceUser
+			instance.latitud=item['lat']
+			instance.longitud=item['lon']
+			instance.altitud=item['alto']
+			instance.charla=item['voz']
+			instance.fechahora=item['hora']
+		#instance.fechaHora=datetime.strptime("'"+item["hora"]+"'", ))
+		#instance.fechaHora=parse_datetime(item['hora'])
+			print instance.fechahora
+			instance.save()
+		resp={'codigo':'ruta','estatus':'ok'}
+	except:
+		resp={'codigo':'ruta','estatus':'fallo'}
+
+	print data
+	print resp
+	return HttpResponse(json.dumps(resp),content_type="application/json")
+
