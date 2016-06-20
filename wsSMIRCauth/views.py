dbb2b0a2e6dd2a96642146ceb1081406eab03464
Diana Saldaña <dianasaldana@MacBook-Pro-de-Diana-Saldana.local>
2016-06-19 22:07:23 -0500

    Con mapas

--- wsSMIRCauth/views.py
+++ wsSMIRCauth/views.py
@@ -54,37 +54,6 @@ def wsLogout(request):
 	resp={'codigo':'logout'}
 	return HttpResponse(json.dumps(resp), content_type="application/json")
 
-def wsRutaMaps(request):
-	data=json.loads(request.body)
-	instanceUser=InicioUserapp.objects.all().get(id=data['id'],email=data['usuario'])
-
-	if instanceUser:
-		routeCoords=[]
-		for item in data['locations']:
-			routeCoords.append({'lon': item['lon'], 'lat': item['lat']})
-
-	print routeCoords
-
-	mapasCode="""
-		function initMap() {
-  			var map = new google.maps.Map(document.getElementById('mapCanvas'), {
-    		zoom: 12,
-    		center: {lat: 0, lng: -180},
-   		mapTypeId: google.maps.MapTypeId.MAPROAD
-   		});
-		var flightPath = new google.maps.Polyline({
-    	path: [%s],
-    	geodesic: true,
-    	strokeColor: '#FA5882',
-    	strokeOpacity: 1.0,
-    	strokeWeight: 2
-  		});
-		flightPath.setMap(map);
-		}
-		"""% (routeCoords)
-
-	return render(request,"RutasMaps.html",{'inicializar':mapasCode})
-
 @csrf_exempt
 def saveDatatoRoute(request):
 	try:
@@ -103,11 +72,53 @@ def saveDatatoRoute(request):
 		#instance.fechaHora=parse_datetime(item['hora'])
 			print instance.fechahora
 			instance.save()
-		resp={'codigo':'ruta','estatus':'ok'}
+			uri="http://wsmirc.herokuapp.com/userRoute/?usuario={0}&fecha={1}".format(data['usuario'],date.today().iso_format())
+		resp={'codigo':'ruta','estatus':'ok', 'uri':uri}
 	except:
 		resp={'codigo':'ruta','estatus':'fallo'}
 
 	print data
 	print resp
 	return HttpResponse(json.dumps(resp),content_type="application/json")
+
+def showMap(request):
+
+	usuario=request.GET['usuario']
+	fecha=request.GET['fecha']
+	instanceUser=InicioUserapp.objects.all().get(email=usuario)
+
+	ubicaciones=list(InicioLocalization.objects.raw('select id,latitud,longitud from inicio_localization where usuario_id=%s and "fechaHora"::timestamp::date=%s group by id,latitud,longitud order by id',[instanceUser.id,fecha])) 
+	
+	mapsCode="""
+	<script>
+		var map;
+      	function initMap() {
+        	var mapOptions = {
+          				center: new google.maps.LatLng(%s, %s),
+          				zoom: 15,
+          				mapTypeId: google.maps.MapTypeId.ROADMAP
+        			};
+        	var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+					"""%(ubicaciones[0].latitud,ubicaciones[0].longitud)
+	
+	pathElement = []
+	for locate in ubicaciones:
+		pathElement.append('new google.maps.LatLng(%s, %s)' % (locate.latitud, locate.longitud))
+	
+	pathElement = ', '.join(pathElement)
+	polylineJavaScript = """var routePath = new google.maps.Polyline({path: [%s],
+					strokeColor: '#FA5882',
+					strokeOpacity: 1.0,
+					strokeWeight: 2
+					});
+routePath.setMap(map);""" % (pathElement)
+	
+	mapsCode= mapsCode + "\n"+polylineJavaScript +"""
+	}
+	</script>"""
+	print mapsCode
+
+	return render(request,"RutaMaps.html",{'instancia':mapsCode})
+
+
 
